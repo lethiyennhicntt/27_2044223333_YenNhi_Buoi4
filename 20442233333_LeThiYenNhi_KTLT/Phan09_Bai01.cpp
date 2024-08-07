64bc64f037d5a3e48c93c30acfb98fe2cfda08dd
KTLT_Buoi5 <len636759@gmail.com>
2024-08-07 13:49:23 +0700

    Buoi 5

---
+++ 20442233333_LeThiYenNhi_KTLT/Phan09_Bai01.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include<math.h>
+// Hàm đệ quy giải bài toán tháp Hà Nội
+void TowerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
+	if (n == 1) {
+		printf("Chuyen dia 1 tu cot %c sang cot %c\n", from_rod, to_rod);
+		return;
+	}
+	TowerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+	printf("Chuyen dia %d tu cot %c sang cot %c\n", n, from_rod, to_rod);
+	TowerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+int main() {
+	int n; // Số lượng đĩa
+	printf("Nhap so luong dia: ");
+	scanf_s("%d", &n);
+	// Gọi hàm TowerOfHanoi với số lượng đĩa và tên các cột		n	0	int
+
+	TowerOfHanoi(n, 'A', 'C', 'B');
+	return 0;
+}
